9a0328f8c0103afdbef4c9c5d7c18502dffe1865
Ismatou <ismatousadio.souare@edu.univ-paris13.fr>
2023-01-31 00:11:00 +0100

    creation du fichier main.py avec class get put

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python_fast_Api
+
+```
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer:Union[bool,None]=None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/somme/{a,b}")
+def somme(a:int,b:int):
+    return{"somme=":a+b}
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int,item_Obj:Item):
+    return {"item name":item_Obj.name, "item id":item_id}
+
